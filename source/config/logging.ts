b69ed28e1f2af1e0b146f4b910b0fe1c425556fc
bolatah <68897346+bolatah@users.noreply.github.com>
2022-04-20 16:09:04 +0200

    Update logging.ts
    one of the argument is changed in console.functions

--- source/config/logging.ts
+++ source/config/logging.ts
@@ -1,41 +1,54 @@
 const getTimeStamp = (): string => {
-    return new Date().toISOString();
+  return new Date().toISOString();
 };
 
 const info = (namespace: string, message: string, object?: any) => {
-    if (object) {
-        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${namespace}`, object);
-    } else {
-        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${namespace}`);
-    }
+  if (object) {
+    console.info(
+      `[${getTimeStamp()}] [INFO] [${namespace}] ${message}`,
+      object
+    );
+  } else {
+    console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+  }
 };
 
 const warn = (namespace: string, message: string, object: any) => {
-    if (object) {
-        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${namespace}`, object);
-    } else {
-        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${namespace}`);
-    }
+  if (object) {
+    console.warn(
+      `[${getTimeStamp()}] [WARN] [${namespace}] ${message}`,
+      object
+    );
+  } else {
+    console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
+  }
 };
 
 const error = (namespace: string, message: string, object: any) => {
-    if (object) {
-        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${namespace}`, object);
-    } else {
-        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${namespace}`);
-    }
+  if (object) {
+    console.error(
+      `[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`,
+      object
+    );
+  } else {
+    console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+  }
 };
 
 const debug = (namespace: string, message: string, object: any) => {
-    if (object) {
-        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${namespace}`, object);
-    } else {
-        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${namespace}`);
-    }
+  if (object) {
+    console.debug(
+      `[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`,
+      object
+    );
+  } else {
+    console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+  }
 };
 export default {
-    info,
-    warn,
-    error,
-    debug
+  info,
+  warn,
+  error,
+  debug,
 };
+
